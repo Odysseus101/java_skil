3d3c1fc751c7ea8f6288de8eb0316c9c4fd9174e
Odysseus101 <35274703+Odysseus101@users.noreply.github.com>
2020-10-30 00:35:22 +0000

    fix

--- Heima_dæmi/Heima_dæmi_9/Fibonacci.java
+++ Heima_dæmi/Heima_dæmi_9/Fibonacci.java
@@ -1,16 +1,18 @@
-public class daemi 
+public class Fibonacci
 {
     public static void main(String[] args)
     {
         int k, a=1, b=1;
+        int n = Integer.parseInt(args[0]);
         k=0;
         System.out.print("1 1 ");
-        while(k<=50)
+        while(k<=n)
         {
             k=a+b;
             System.out.print(k + " ");
             a=b;
             b=k;
+            
         }
     }
 }
