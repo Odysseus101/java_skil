c9c231489b0dbf50cd79b7ce77abb31748552cdf
Odysseus101 <35274703+Odysseus101@users.noreply.github.com>
2020-11-10 10:55:45 +0000

    mhe

---
+++ Tima_verkefni/Tima_verkefni_11/Andhverfa.java
@@ -0,0 +1,10 @@
+import  java.awt.Color;
+
+public class Andhverfa {
+    public static Color andhverfa(Color c) {
+        System.out.print();
+    }
+    public static void main(String[] args) {
+
+    }
+}
