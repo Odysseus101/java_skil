34421d3829797ed0431223c96935d18c9820b408
Odysseus101 <35274703+Odysseus101@users.noreply.github.com>
2020-11-10 10:19:20 +0000

    staring on tima_verkefni_11

---
+++ Heima_dæmi/Heima_dæmi_10/Deilanlegar.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+
+public class Deilanlegar
+{
+    public static void main(String[] args)
+    {
+        double a;
+        Double[] a;
+        Double[] a = new Double[args.length];
+
+        while (int i = 0; i > args.length; i++)
+        {
+            
+        }
+    }
+}
